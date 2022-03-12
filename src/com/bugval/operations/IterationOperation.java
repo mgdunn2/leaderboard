0870ed90fed90657d856978387eccd50b46f8b1f
Mike Dunn <mdunn@yext.com>
2022-03-12 18:54:04 -0500

    format all code

--- src/com/bugval/operations/IterationOperation.java
+++ src/com/bugval/operations/IterationOperation.java
@@ -10,63 +10,63 @@ import java.util.List;
 import java.util.Optional;
 
 public class IterationOperation {
-    private static final int NUM_OF_INTEREST = 6;
+  private static final int NUM_OF_INTEREST = 6;
 
-    private final ScoreRepo scoreRepo;
-    private final ScoreFetcher fetcher;
+  private final ScoreRepo scoreRepo;
+  private final ScoreFetcher fetcher;
 
-    @Inject
-    public IterationOperation(ScoreRepo scoreRepo, ScoreFetcher fetcher) {
-        this.scoreRepo = scoreRepo;
-        this.fetcher = fetcher;
+  @Inject
+  public IterationOperation(ScoreRepo scoreRepo, ScoreFetcher fetcher) {
+    this.scoreRepo = scoreRepo;
+    this.fetcher = fetcher;
+  }
+
+  public void handleIteration() {
+    List<Score> newScores = fetcher.fetchScores();
+    if (scoreRepo.getScores().isEmpty()) {
+      scoreRepo.saveScores(newScores);
+      newScores.stream()
+          .sorted(Comparator.comparing(Score::getPosition))
+          .limit(NUM_OF_INTEREST)
+          .forEach(
+              s ->
+                  System.out.printf(
+                      "Player %s starting in position %d with %d points\n",
+                      s.getId(), s.getPosition(), s.getScore()));
+      System.out.println();
+      return;
     }
 
-    public void handleIteration() {
-        List<Score> newScores = fetcher.fetchScores();
-        if (scoreRepo.getScores().isEmpty()) {
-            scoreRepo.saveScores(newScores);
-            newScores.stream()
-                    .sorted(Comparator.comparing(Score::getPosition))
-                    .limit(NUM_OF_INTEREST)
-                    .forEach(
-                            s ->
-                                    System.out.printf(
-                                            "Player %s starting in position %d with %d points\n",
-                                            s.getId(), s.getPosition(), s.getScore()));
-            System.out.println();
-            return;
-        }
-
-        newScores.stream()
-                .sorted(Comparator.comparing(Score::getPosition))
-                .limit(NUM_OF_INTEREST)
-                .flatMap(
-                        s -> {
-                            if (!scoreRepo.getScoresByPlayer().containsKey(s.getId())) {
-                                return Optional.of(
-                                        String.format(
-                                                "New Player %s moved into position %d with %d points",
-                                                s.getId(), s.getPosition(), s.getScore()))
-                                        .stream();
-                            } else if (scoreRepo.getScoresByPlayer().get(s.getId()).getPosition()
-                                    > s.getPosition()) {
-                                return Optional.of(
-                                        String.format(
-                                                "Player %s moved up to position %d with %d points",
-                                                s.getId(), s.getPosition(), s.getScore()))
-                                        .stream();
-                            } else if (scoreRepo.getScoresByPlayer().get(s.getId()).getPosition()
-                                    < s.getPosition()) {
-                                return Optional.of(
-                                        String.format(
-                                                "Player %s moved down to position %d with %d points",
-                                                s.getId(), s.getPosition(), s.getScore()))
-                                        .stream();
-                            }
-                            return Optional.empty().stream();
-                        })
-                .forEach(System.out::println);
-        System.out.println();
-        scoreRepo.saveScores(newScores);
-    }
+    newScores.stream()
+        .sorted(Comparator.comparing(Score::getPosition))
+        .limit(NUM_OF_INTEREST)
+        .flatMap(
+            s -> {
+              if (!scoreRepo.getScoresByPlayer().containsKey(s.getId())) {
+                return Optional.of(
+                    String.format(
+                        "New Player %s moved into position %d with %d points",
+                        s.getId(), s.getPosition(), s.getScore()))
+                    .stream();
+              } else if (scoreRepo.getScoresByPlayer().get(s.getId()).getPosition()
+                  > s.getPosition()) {
+                return Optional.of(
+                    String.format(
+                        "Player %s moved up to position %d with %d points",
+                        s.getId(), s.getPosition(), s.getScore()))
+                    .stream();
+              } else if (scoreRepo.getScoresByPlayer().get(s.getId()).getPosition()
+                  < s.getPosition()) {
+                return Optional.of(
+                    String.format(
+                        "Player %s moved down to position %d with %d points",
+                        s.getId(), s.getPosition(), s.getScore()))
+                    .stream();
+              }
+              return Optional.empty().stream();
+            })
+        .forEach(System.out::println);
+    System.out.println();
+    scoreRepo.saveScores(newScores);
+  }
 }
